693402cbbdd088bfe8d65d1b5e7666099a6060cb
Omar-Trigui <34484469+Omar-Trigui@users.noreply.github.com>
2019-12-16 23:13:28 +0100

    Order Service

--- Orders/Orders.js
+++ Orders/Orders.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const axios = require("axios");
 require("./OrderModel");
 app.use(bodyParser.json());
 
@@ -13,33 +14,66 @@ mongoose.connect(
   }
 );
 
-app.post('/order' , (req,res) => {
-    var newOrder ={
-        CustomerID : mongoose.Types.ObjectId(req.body.CustomerID) ,
-        BookID : mongoose.Types.ObjectId(req.body.BookID) ,
-        InitialDate : req.body.InitialDate,
-        deliveryDate : req.body.deliveryDate
-    }
-    var order = new Order(newOrder)
-    order.save().then(() => {
-        res.send('order created');
-        
-    }).catch(err => {
-        if(err) {
-            throw err ;
-        }
+app.post("/order", (req, res) => {
+  var newOrder = {
+    CustomerID: mongoose.Types.ObjectId(req.body.CustomerID),
+    BookID: mongoose.Types.ObjectId(req.body.BookID),
+    InitialDate: req.body.InitialDate,
+    deliveryDate: req.body.deliveryDate
+  };
+  var order = new Order(newOrder);
+  order
+    .save()
+    .then(() => {
+      res.send("order created");
     })
-    
-})
-app.get('/orders', (req,res) => {
-    Order.find().then((data) => {
-        res.send(data)
-    }).catch(err => {
-        if(err){
-            throw err;
-        }
+    .catch(err => {
+      if (err) {
+        throw err;
+      }
+    });
+});
+app.get("/orders", (req, res) => {
+  Order.find()
+    .then(data => {
+      res.send(data);
     })
-})
+    .catch(err => {
+      if (err) {
+        throw err;
+      }
+    });
+});
+
+app.get("/order/:id", (req, res) => {
+  Order.findById(req.params.id)
+    .then(order => {
+      if (order) {
+        axios
+          .get("http://127.0.0.1:5555/customer/" + order.CustomerID)
+          .then(response => {
+            var orderObject = {
+              customerName: response.data.name,
+              bookTitle: ""
+            };
+            axios
+              .get("http://127.0.0.1:4545/book/" + order.BookID)
+              .then(response => {
+                orderObject.bookTitle = response.data.Title;
+                res.json(orderObject);
+                
+              });
+          });
+      } else {
+        res.send("invalid order");
+      }
+    })
+    .catch(err => {
+      if (err) {
+        throw err;
+      }
+    });
+});
 const Order = mongoose.model("Order");
 app.listen(7777, () => {
   console.log("service Order is running");
